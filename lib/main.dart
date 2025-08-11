354e5f6646f26e8de8eb7da5245ef237be9ff22b
Subhajit Roy <darrang48@gmail.com>
2025-08-11 12:53:49 +0530

    Only portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:ednect/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
